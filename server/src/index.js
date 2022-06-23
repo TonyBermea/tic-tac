4c35cb416a02bb5c43184e2eb46f79e36005b196
TonyBermea <tony.bermea@icloud.com>
2022-06-23 08:13:14 -0700

    first

---
+++ server/src/index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import cors from "cors";
+import { StreamChat } from "stream-chat";
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const api_key = '3gcm3cdcnch7';
+const api_secret = 'rb4grzf7gpy2hfj9enzgkpb8tr9d6jx6fbwnv5nq4xj8vz5a78wesartn4nkp92b';
+const serverClient = new StreamChat.getInstance(api_key, api_secret);
+
+app.post("/signup", async (req, res) => {
+  try {
+    const { firstName, lastName, username, password } = req.body;
+    const userId = uuidv4();
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const token = serverClient.createToken(userId);
+    res.json({ token, userId, firstName, lastName, username, hashedPassword });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const { users } = await serverClient.queryUsers({ name: username });
+    if (users.length === 0) return res.json({ message: "User not found" });
+
+    const token = serverClient.createToken(users[0].id);
+    const passwordMatch = await bcrypt.compare(
+      password,
+      users[0].hashedPassword
+    );
+
+    if (passwordMatch) {
+      res.json({
+        token,
+        firstName: users[0].firstName,
+        lastName: users[0].lastName,
+        username,
+        userId: users[0].id,
+      });
+    }
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
